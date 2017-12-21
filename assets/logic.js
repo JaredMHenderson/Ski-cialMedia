6037a9f29cc7b2464bec3b637269048cacfcfb22
Jared Henderson <jaredmhenderson@icloud.com>
2017-12-20 19:48:19 -0700

    fixed geolocation issue

--- assets/logic.js
+++ assets/logic.js
@@ -1,20 +1,18 @@
 $(function(){
 
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCGFZJ4_F1ujKk_g57Xc0npkyrComAgsMg",
+        authDomain: "ski-resort-app.firebaseapp.com",
+        databaseURL: "https://ski-resort-app.firebaseio.com",
+        projectId: "ski-resort-app",
+        storageBucket: "",
+        messagingSenderId: "195192287950"
+    };
 
+    
 
-// 
-
-// let weatherOnline = "http://api.worldweatheronline.com/free/v1/";
-// let apiKey = "68115c9b7784fad87c21511172012 ";
-// let queryUrl = 
-// $.ajax({
-//     url: queryUrl,
-//     method: "GET"
-// }).done(function(response){
-//     console.log(response);
-// });
-
-
+//Animtes text input box when "submit" button is clicked
 $("#searchButton").on("click", function(){
     console.log("clicked");
     $('input[name=searchBox]').removeClass("animated fadeInUp").addClass("animated shake").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
@@ -24,8 +22,7 @@ $("#searchButton").on("click", function(){
 
 });
 
-var x = $('#location');
-
+//Gets current location and displays latitude/longitude in a div
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -43,44 +40,49 @@ function showPosition(position) {
         "<br>Longitude: " + position.coords.longitude);  
 };
 
+//the location function
 getLocation();
 
 
-    var map, infoWindow;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: -34.397, lng: 150.644 },
-            zoom: 6
-        });
-        infoWindow = new google.maps.InfoWindow;
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
+//testing out the Google Maps geolocation feature
 
-                infoWindow.setPosition(pos);
-                infoWindow.setContent('Location found.');
-                infoWindow.open(map);
-                map.setCenter(pos);
-            }, function () {
-                handleLocationError(true, infoWindow, map.getCenter());
-            });
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
-    }
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-            'Error: The Geolocation service failed.' :
-            'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-    }
+    
 
 });
+var map, infoWindow;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 6
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
