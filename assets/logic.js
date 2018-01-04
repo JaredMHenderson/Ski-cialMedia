ab0049d80cb85b1ec023b760a12c98ac3298cea3
Jared Henderson <jaredmhenderson@icloud.com>
2018-01-03 19:54:33 -0700

    added initial onclick functions for state buttons, populates map area

--- assets/logic.js
+++ assets/logic.js
@@ -5,19 +5,60 @@
 
 $(function(){
 
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyCGFZJ4_F1ujKk_g57Xc0npkyrComAgsMg",
-        authDomain: "ski-resort-app.firebaseapp.com",
-        databaseURL: "https://ski-resort-app.firebaseio.com",
-        projectId: "ski-resort-app",
-        storageBucket: "ski-resort-app.appspot.com",
-        messagingSenderId: "195192287950"
-    };
-    firebase.initializeApp(config);
+//on click function for colorado button
+
+$('#colorado-button').on("click",function(){
+    // alert("colorado was clicked");
+    $('#map').html(`<iframe 
+        width="100%" 
+        height="100%" 
+        frameborder="0" 
+        style="border:0" 
+        src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDbyddmrqW7wONDFRt9o54qgXBEcc7lMf8&q=Ski+Resort+Colorado&zoom=7" allowfullscreen>
+        </iframe>`)
 
 });
 
+//on click function for utah button
+
+$('#utah-button').on("click", function(){
+    // alert('utah was clicked');
+    $('#map').html(`<iframe 
+        width="100%" 
+        height="100%" 
+        frameborder="0" 
+        style="border:0" 
+        src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDbyddmrqW7wONDFRt9o54qgXBEcc7lMf8&q=Ski+Resort+Utah&zoom=7" allowfullscreen>
+        </iframe>`)
+});
+
+//on click function for central CA button
+
+$('#central-california-button').on("click", function () {
+        // alert('CA was clicked');
+        $('#map').html(`<iframe 
+        width="100%" 
+        height="100%" 
+        frameborder="0" 
+        style="border:0" 
+        src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDbyddmrqW7wONDFRt9o54qgXBEcc7lMf8&q=Ski+Resort,Central+California&zoom=7" allowfullscreen>
+        </iframe>`)
+    });
+
+
+    //on click function for northern CA button
+$('#northern-california-button').on("click", function () {
+        // alert('CA was clicked');
+        $('#map').html(`<iframe 
+        width="100%" 
+        height="100%" 
+        frameborder="0" 
+        style="border:0" 
+        src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDbyddmrqW7wONDFRt9o54qgXBEcc7lMf8&q=Ski+Resort,Northern+California&zoom=7" allowfullscreen>
+        </iframe>`)
+    });
+
+});
 
 
 
