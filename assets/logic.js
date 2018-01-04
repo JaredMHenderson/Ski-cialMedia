4ffc30941fa9100983022fb747612891ff4b00a4
Jared Henderson <jaredmhenderson@icloud.com>
2018-01-04 08:01:18 -0700

    started basic animation, started adding buttons

--- assets/logic.js
+++ assets/logic.js
@@ -2,6 +2,10 @@
 // Research how to share a firebase database
 
 
+// array of resorts that show up when Colorado Button is clicked
+let coloradoResorts = ["Keystone", "Copper Mountain", "Loveland", "Monarch", "Arapahoe Basin"]
+
+
 
 $(function(){
 
@@ -9,27 +13,35 @@ $(function(){
 
 $('#colorado-button').on("click",function(){
     // alert("colorado was clicked");
+    // $('#map').addClass('fadeIn');
+    $("#list").empty();
+    for (var i = 0; i < coloradoResorts.length; i++) {
+
+        $("#list").append(`<button data-index='${i}'>${coloradoResorts[i]}</button>`);
+    };
+
     $('#map').html(`<iframe 
         width="100%" 
         height="100%" 
         frameborder="0" 
         style="border:0" 
         src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDbyddmrqW7wONDFRt9o54qgXBEcc7lMf8&q=Ski+Resort+Colorado&zoom=7" allowfullscreen>
-        </iframe>`)
-
+        </iframe>`);
 });
 
 //on click function for utah button
 
 $('#utah-button').on("click", function(){
     // alert('utah was clicked');
+    
+
     $('#map').html(`<iframe 
         width="100%" 
         height="100%" 
         frameborder="0" 
         style="border:0" 
         src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDbyddmrqW7wONDFRt9o54qgXBEcc7lMf8&q=Ski+Resort+Utah&zoom=7" allowfullscreen>
-        </iframe>`)
+        </iframe>`);
 });
 
 //on click function for central CA button
@@ -42,7 +54,7 @@ $('#central-california-button').on("click", function () {
         frameborder="0" 
         style="border:0" 
         src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDbyddmrqW7wONDFRt9o54qgXBEcc7lMf8&q=Ski+Resort,Central+California&zoom=7" allowfullscreen>
-        </iframe>`)
+        </iframe>`);
     });
 
 
@@ -55,7 +67,7 @@ $('#northern-california-button').on("click", function () {
         frameborder="0" 
         style="border:0" 
         src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDbyddmrqW7wONDFRt9o54qgXBEcc7lMf8&q=Ski+Resort,Northern+California&zoom=7" allowfullscreen>
-        </iframe>`)
+        </iframe>`);
     });
 
 });
