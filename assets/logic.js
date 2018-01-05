2d5c334cfb7528a3fe2fa9137e6b7d847da359dc
Jared Henderson <jaredmhenderson@icloud.com>
2018-01-05 10:06:03 -0700

    added a few resorts

--- assets/logic.js
+++ assets/logic.js
@@ -2,16 +2,13 @@
 // Research how to share a firebase database
 
 
-// array of resorts that show up when each state Button is clicked
-let coloradoResorts = ["Keystone", "Copper Mountain", "Loveland", "Monarch", "Arapahoe Basin"];
-let utahResorts = ["Beaver Mountain", "Brighton Ski Resort", "Dear Valley", "Sundance Resort", "Solitude Mountain" ];
-let centralCaResorts = ["Mammoth Mountain", "Badger Pass", "Dodge Ridge", "China Peak"];
-let northernCaResorts = ["Bear Valley", "Boreal Mountain Resort", "Dodge Ridge", "Donner Ski Ranch", "Heavenly Mountain"];
-
-
-
-
 $(function(){
+
+    // array of resorts that show up when each state Button is clicked
+    let coloradoResorts = ["Keystone", "Copper Mountain", "Loveland", "Monarch", "Arapahoe Basin", "Crested Butte", "Vail"];
+    let utahResorts = ["Beaver Mountain", "Brighton Ski Resort", "Dear Valley", "Sundance Resort", "Solitude Mountain"];
+    let centralCaResorts = ["Mammoth Mountain", "Badger Pass", "Dodge Ridge", "China Peak"];
+    let northernCaResorts = ["Bear Valley", "Boreal Mountain Resort", "Dodge Ridge", "Donner Ski Ranch", "Heavenly Mountain"];
 
 //on click function for colorado button
 
@@ -19,12 +16,13 @@ $('#colorado-button').on("click",function(){
     // alert("colorado was clicked");
     $("#list").empty();
     $("#list").append(`<h1 class="animated fadeIn">Colorado Resorts</h1>`);
-    $("")
+    $('#list').css('background-image', 'url("./assets/images/jorg-angeli-406235.jpg")');
 
     for (var i = 0; i < coloradoResorts.length; i++) {
 
-        $("#list").append(`<div><button class="animated fadeInUp" data-index='${i}'>${coloradoResorts[i]}</button><div>`);
+        $("#list").append(`<div><button class="animated fadeInUp" id='${coloradoResorts[i]}'>${coloradoResorts[i]}</button><div>`);
     };
+    
 
     $('#map').html(`<iframe 
         width="100%" 
