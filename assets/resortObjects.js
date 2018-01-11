10273b9246b8494fa94fd92d9611e953d9f3f085
Michael Abeyta <mabeyta1990@gmail.com>
2018-01-10 20:58:28 -0700

    ./

---
+++ assets/resortObjects.js
@@ -0,0 +1,108 @@
+// COLORADO RESORT OBJECTS
+let keystone = {
+    name: "Keystone",
+    id: "keystone",
+}
+let copperMountain = {
+    name: "Copper Mountain",
+    id: "copper",
+}
+let loveland = {
+    name: "Loveland",
+    id: "loveland",
+}
+let monarch = {
+    name: "Monarch",
+    id: "monarch",
+}
+let winterPark = {
+    name: "Winter Park",
+    id: "winter-park",
+}
+let arapahoBasin = {
+    name: "Arapaho Basin",
+    id: "abasin",
+}
+let crestedButte = {
+    name: "Crested Butte",
+    id: "crested-butte",
+}
+let vail = {
+    name: "Vail",
+    id: "vail",
+}
+//UTAH RESORT OBJECTS
+let beaverMountain = {
+    name: "Beaver Mountain",
+    id: "doesNotExist",
+}
+let brightonSkiResort = {
+    name: "Brighton Ski Resort",
+    id: "brighton",
+}
+let deerValley = {
+    name: "Deer Valley",
+    id: "deer-valley",
+}
+let sundanceResort = {
+    name: "Sundance Resort",
+    id: "doesNotExist",
+}
+let solitudeMountain = {
+    name: "Solitude Mountain",
+    id: "solitude",
+}
+let powderMountain = {
+    name: "Powder Mountain",
+    id: "doesNotExist",
+}
+let parkCityMountain = {
+    name: "Park City Mountain",
+    id: "parkcity",
+}
+let wolfMountain = {
+    name: "Wolf Mountain",
+    id: "doesNotExist",
+}
+//CENTRAL CA RESORT OBJECTS
+let mammothMountain = {
+    name: "Mammoth Mountain",
+    id: "mammoth-lakes",
+}
+let badgerPass = {
+    name: "Badger Pass",
+    id: "doesNotExist",
+}
+let dodgeRidge = {
+    name: "Dodge Ridge",
+    id: "doesNotExist",
+}
+let chinaPeak = {
+    name: "China Peak",
+    id: "chinapeak",
+}
+//NORTHERN CA RESORT OBJECTS
+let bearValley = {
+    name: "Bear Valley",
+    id: "doesNotExist",
+}
+let northStar = {
+    name: "Northstar California",
+    id: "northstar",
+}
+let sugarBowlSkiResort = {
+    name: "Sugar Bowl Ski Resort",
+    id: "sugarbowl",
+}
+let borealMountainResort = {
+    name: "Boreal Mountain Resort",
+    id: "boreal",
+}
+let donnerSkiRanch = {
+    name: "Donner Ski Ranch",
+    id: "tahoe-donner",
+}
+let heavenlyMountain = {
+    name: "Heavenly Mountain",
+    id: "heavenly",
+}
